5ba56c21f8a27ec5103c8ab619d3fbc59db3256d
JoeyDallaire <32402889+JoeyDallaire@users.noreply.github.com>
2022-02-05 22:59:45 -0500

    Update cameraScript.py

--- cameraScript.py
+++ cameraScript.py
@@ -8,10 +8,10 @@ debug = True
 picLed = LED(27)
 
 camera = PiCamera()
-path = '/home/pi/Desktop/plantes/'
+path = '/home/pi/Desktop/plantes/' # The path of the folder you want your jpegs to be saved in.
 initWaitTime = 5
-waitTime = 5 #900 # 15 minutes
-maxPics = 6720
+waitTime = 900 # This is the time between every frame, I made it 15min for my 10 weeks timelapse.
+maxPics = 6720 # The max number of picture you want to take in total, check how much space you have left.
 
 def debugPrint(prtStr):
     if debug:
@@ -22,7 +22,8 @@ def mainLoop(time):
     while i < time:
         now = datetime.now()
         picLed.on()
-        camera.capture(path + now.strftime('%m-%d%Y %H-%M-%S') + '.jpg')
+        # I used a datetime format for the files name so it's easier to do the editing later.
+        camera.capture(path + now.strftime('%m-%d%Y %H-%M-%S') + '.jpg') 
         picLed.off()
         debugPrint('Picute #' + str(i+1) + ' taken.')
         sleep(waitTime)
@@ -30,6 +31,8 @@ def mainLoop(time):
 
 
 def startIt():
+    
+    # I have commented out the camera previews because it takes all your screen if it happens hold crt+al+t and blindly type "pkill python3"
     #camera.start_preview()
     sleep(initWaitTime)
     mainLoop(maxPics)
