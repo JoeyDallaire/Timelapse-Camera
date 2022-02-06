116c09f817a55327de8f5498baa14146f5e6c2a0
JoeyDallaire <32402889+JoeyDallaire@users.noreply.github.com>
2022-02-05 22:48:26 -0500

    Add files via upload

--- cameraScript.py
+++ cameraScript.py
@@ -1,16 +1,40 @@
 from datetime import datetime
-path = '/home/pi/desktop/plantes/'
-initWaitTime = 5
-waitTime = 2
+from time import sleep
+from picamera import PiCamera
+from gpiozero import LED
 
-def mainLoop():
-    sleep(waitTime)
-    now = datetime.now()
-    camera.capture(path + now.strftime('%m-%d%Y %H-%M-%S') + '.jpg')
+debug = True
+
+picLed = LED(27)
+
+camera = PiCamera()
+path = '/home/pi/Desktop/plantes/'
+initWaitTime = 5
+waitTime = 5 #900 # 15 minutes
+maxPics = 6720
+
+def debugPrint(prtStr):
+    if debug:
+        print(prtStr)
+
+def mainLoop(time):
+    i=0
+    while i < time:
+        now = datetime.now()
+        picLed.on()
+        camera.capture(path + now.strftime('%m-%d%Y %H-%M-%S') + '.jpg')
+        picLed.off()
+        debugPrint('Picute #' + str(i+1) + ' taken.')
+        sleep(waitTime)
+        i+=1
 
 
 def startIt():
-    camera.start_preview()
+    #camera.start_preview()
     sleep(initWaitTime)
-    mainLoop()
-    camera.stop_preview()
+    mainLoop(maxPics)
+    #camera.stop_preview()
+    debugPrint('done')
+    
+
+startIt()
